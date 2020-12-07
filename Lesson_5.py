d48d6119f8591ec523fb4c04fb826ae67362b4ed
plv88 <insta1988PpLlVv>
2020-12-07 14:39:49 +0300

    Lesson_05: exercise_2 Сдалел 2 разными способами почитать. Файл с данными - exercise_2_test.txt

--- Lesson_5.py
+++ Lesson_5.py
@@ -1,11 +1,17 @@
-# 1. Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
-# Об окончании ввода данных свидетельствует пустая строка.
+# 2. Создать текстовый файл (не программно), сохранить в нем несколько строк, выполнить подсчет количества строк,
+# количества слов в каждой строке.
 
-data = open("data_test.txt", "w")
-while(True):
-    input_string = input("Введите данные")
-    if input_string == "":
-        break
-    data.writelines(input_string+'\n')
-data.close()
-print("Запись завершена, можно посмотреть что записали")
+f_obj = open("exercise_2_test.txt", "r")
+content = f_obj.read()
+content_str = content.split("\n")
+print(f"Количество строк {len(content_str)}")
+f_obj.close()
+print()
+f_obj = open("exercise_2_test.txt", "r")
+n = 1
+for line in f_obj:
+    number_word = len(line.split(" "))
+    line = line.replace("\n", "")
+    print(f"Cтрока {n} - {line} - слов: {number_word}")
+    n +=1
+f_obj.close()
