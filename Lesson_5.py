ffaebf7c635391521694a808135c1c57bad656f3
plv88 <insta1988PpLlVv>
2020-12-07 19:18:44 +0300

    Lesson_05: exercise_3 Сделал через циклы, так показалось проще + данных не так много, поэтому на скорость это не сильно влияет

--- Lesson_5.py
+++ Lesson_5.py
@@ -1,17 +1,27 @@
-# 2. Создать текстовый файл (не программно), сохранить в нем несколько строк, выполнить подсчет количества строк,
-# количества слов в каждой строке.
-
-f_obj = open("exercise_2_test.txt", "r")
-content = f_obj.read()
-content_str = content.split("\n")
-print(f"Количество строк {len(content_str)}")
-f_obj.close()
-print()
-f_obj = open("exercise_2_test.txt", "r")
-n = 1
-for line in f_obj:
-    number_word = len(line.split(" "))
+# 3. Создать текстовый файл (не программно), построчно записать фамилии сотрудников и величину их окладов. Определить,
+# кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников. Выполнить подсчет средней величины
+# дохода сотрудников.
+#Записал на английском, в задании не было русские\английский поэтому чтобы не возиться с кодировкой, сделал английские.
+my_employ = open("employees.txt", "r")
+#Запишем все в словарик, так проще будет работать
+individual_dict = {}
+for line in my_employ:
     line = line.replace("\n", "")
-    print(f"Cтрока {n} - {line} - слов: {number_word}")
-    n +=1
-f_obj.close()
+    array_employ = line.split(" ")
+    individual_dict[array_employ[0]] = array_employ[1]
+sum = 0
+n = 0
+for keys, values in individual_dict.items():
+    if int(values) < 20000:
+        sum += int(values)
+        n += 1
+        print(keys)
+print(f"Средняя тех кто зарабатывает меньше 20к - {int(sum/n)}")
+sum = 0
+n = 0
+for keys, values in individual_dict.items():
+    sum += int(values)
+    n += 1
+print(f"Средняя всех - {int(sum/n)}")
+
+my_employ.close()
