7c29360eafe7fa1458c465c7de89cc2967dec056
plv88 <insta1988PpLlVv>
2020-12-16 00:10:10 +0300

    Lesson_07: Ex2. Странное задание, решил 2мя способами, так ли нужно было, жду пояснения)

--- Less0n_7.py
+++ Less0n_7.py
@@ -8,7 +8,7 @@
 # Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки первой матрицы складываем с
 # первым элементом первой строки второй матрицы и т.д.
 
-#так проще сложить списки, с циклами просидел несколько часов
+# #так проще сложить списки, с циклами просидел несколько часов
 from itertools import zip_longest
 
 class Matrix:
@@ -80,3 +80,76 @@ m4 = Matrix([
     [12, 12, 12]
 ])
 print(m3+m4)
+
+# 2. Реализовать проект расчета суммарного расхода ткани на производство одежды. Основная сущность (класс) этого проекта —
+# одежда, которая может иметь определенное название. К типам одежды в этом проекте относятся пальто и костюм. У этих типов
+# одежды существуют параметры: размер (для пальто) и рост (для костюма). Это могут быть обычные числа: V и H, соответственно.
+# Для определения расхода ткани по каждому типу одежды использовать формулы: для пальто (V/6.5 + 0.5), для костюма (2 * H + 0.3).
+# Проверить работу этих методов на реальных данных.
+# Реализовать общий подсчет расхода ткани. Проверить на практике полученные на этом уроке знания: реализовать абстрактные
+# классы для основных классов проекта, проверить на практике работу декоратора @property.
+
+
+class Clothes():
+    def __init__(self, title):
+        self.title = title
+        print(f"Название линейки одежды - {self.title}")
+
+    def coat(self, size):
+        self.size = int(size)
+        self.coat_expense = self.size / 6.5 + 0.5
+        return self.coat_expense
+
+    def costume(self, growth):
+        self.growth = growth
+        self.costume_expense = 2 * self.growth + 0.3
+        return self.costume_expense
+
+    @property
+    def expense(self):
+        return f"Суммарный расход ткани состовляет: {self.coat_expense + self.costume_expense} ед."
+
+wear = Clothes("Зима 2020")
+print(wear.coat(250))
+print(wear.costume(165))
+print(wear.expense)
+
+print("________________________________________")
+
+from abc import ABC, abstractmethod
+
+class Clothes_abc(ABC):
+    def __init__(self, title, par=None):
+        self.title = title
+        self.par = par
+
+    @abstractmethod
+    def calc_expense(self):
+        pass
+
+class Coat(Clothes_abc):
+    @property
+    def calc_expense(self):
+        self.coat_expense = (self.par / 6.5) + 0.5
+        return self.coat_expense
+
+class Costume(Clothes_abc):
+    @property
+    def calc_expense(self):
+        self.costume_expense = 2 * self.par + 0.3
+        return self.costume_expense
+
+class Sum_expense():
+    def __init__(self, par1, par2):
+        self.par1 = par1
+        self.par2 = par2
+
+    def calc_expense(self):
+        return self.par1 + self.par2
+
+coat_abc = Coat("Пальто", 250)
+print(coat_abc.calc_expense)
+costume_abc = Costume("Зима 2021", 165)
+print(costume_abc.calc_expense)
+expense_all = Sum_expense(costume_abc.calc_expense, coat_abc.calc_expense)
+print(expense_all.calc_expense())
